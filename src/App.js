82416512b3078e1032e07660c15351111ec54be8
Yulia kolomiets <yuliakolomiets09@gmail.com>
2024-12-12 23:30:27 +0300

    feat: install json-server and get pizzas data from json-server #6 lesson

--- src/App.js
+++ src/App.js
@@ -1,15 +1,21 @@
 import React from 'react';
+import axios from 'axios';
 
 import Header from './components/Header';
 import Categories from './components/Categories';
 import Sort from './components/Sort';
 import PizzaBlock from './components/PizzaBlock';
 
-import pizzas from './assets/pizza.json';
-
 import './scss/app.scss';
 
 function App() {
+  const [items, setItems] = React.useState([]);
+
+  React.useEffect(() => {
+    axios
+      .get(`${process.env.REACT_APP_API_URL}/items`)
+      .then((arr) => setItems(arr.data));
+  }, []);
   return (
     <div className="App">
       <div className="wrapper">
@@ -22,7 +28,7 @@ function App() {
             </div>
             <h2 className="content__title">Все пиццы</h2>
             <div className="content__items">
-              {pizzas.map((obj) => (
+              {items.map((obj) => (
                 <PizzaBlock {...obj} />
               ))}
             </div>
