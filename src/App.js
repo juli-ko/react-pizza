72d002352b553eb8863ae933b574f839ccf79831
Yulia kolomiets <yuliakolomiets09@gmail.com>
2024-12-14 13:40:41 +0300

    feat: add react routing and not found component #7 lesson

--- src/App.js
+++ src/App.js
@@ -1,40 +1,26 @@
 import React from 'react';
 import axios from 'axios';
+import { Routes, Route } from 'react-router-dom';
 
 import Header from './components/Header';
-import Categories from './components/Categories';
-import Sort from './components/Sort';
-import PizzaBlock from './components/PizzaBlock';
-import Skeleton from './components/PizzaBlock/Skeleton';
+import Home from './pages/Home';
+import NotFound from './pages/NotFound';
+import Cart from './pages/Cart';
 
 import './scss/app.scss';
 
 function App() {
-  const [items, setItems] = React.useState([]);
-  const [isLoading, setIsLoading] = React.useState(true);
-
-  React.useEffect(() => {
-    axios.get(`${process.env.REACT_APP_API_URL}/items`).then((arr) => {
-      setItems(arr.data);
-      setIsLoading(false);
-    });
-  }, []);
   return (
     <div className="App">
       <div className="wrapper">
         <Header />
         <div className="content">
           <div className="container">
-            <div className="content__top">
-              <Categories />
-              <Sort />
-            </div>
-            <h2 className="content__title">Все пиццы</h2>
-            <div className="content__items">
-              {isLoading
-                ? [...new Array(6)].map((el, i) => <Skeleton key={i} />)
-                : items.map((obj) => <PizzaBlock key={obj.id} {...obj} />)}
-            </div>
+            <Routes>
+              <Route path="/" element={<Home />} />
+              <Route path="/cart" element={<Cart />} />
+              <Route path="*" element={<NotFound />} />
+            </Routes>
           </div>
         </div>
       </div>
