736b12289e32066542a19a9b1f36dd8af04b35e3
Yulia kolomiets <yuliakolomiets09@gmail.com>
2024-12-29 22:42:24 +0300

    feat: optimize search with debounce

--- src/components/Search/index.jsx
+++ src/components/Search/index.jsx
@@ -1,11 +1,34 @@
 import React from 'react';
+import debounce from 'lodash.debounce';
 
 import { SearchContext } from '../../App';
 
 import styles from './Search.module.scss';
 
 function Search() {
-  const { searchValue, setSearchValue } = React.useContext(SearchContext);
+  const [value, setValue] = React.useState('');
+  const { setSearchValue } = React.useContext(SearchContext);
+  const inputRef = React.useRef();
+
+  const onClickClear = () => {
+    setSearchValue('');
+    //bad practice:
+    //document.querySelector('input').focus()
+    inputRef.current.focus();
+  };
+
+  const updateSearchValue = React.useCallback(
+    debounce((str) => {
+      console.log(str, 'lodash');
+      setSearchValue(str);
+    }, 1000),
+    []
+  );
+
+  const onChangeInput = (event) => {
+    setValue(event.target.value);
+    updateSearchValue(event.target.value);
+  };
 
   return (
     <div className={styles.root}>
@@ -18,14 +41,15 @@ function Search() {
         <path d="M0 0h48v48H0z" fill="none" />
       </svg>
       <input
-        value={searchValue} //react controlled input
-        onChange={(event) => setSearchValue(event.target.value)}
+        ref={inputRef}
+        value={value} //react controlled input
+        onChange={onChangeInput}
         className={styles.input}
         placeholder="Поиск пиццы..."
       />
-      {searchValue && (
+      {value && (
         <svg
-          onClick={() => setSearchValue('')}
+          onClick={onClickClear}
           className={styles.clearIcon}
           viewBox="0 0 20 20"
           xmlns="http://www.w3.org/2000/svg"
